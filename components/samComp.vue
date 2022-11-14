7b8e98aa4f51c64e0c71895012c05da4889e0622
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-14 14:03:54 +0530

    Initial Commit

---
+++ components/samComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    This is my Sample Component
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <h1 class="font-bold underline">From inside the div</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
